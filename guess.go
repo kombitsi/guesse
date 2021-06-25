89c54d967c5bacefee82e227aed31e4b24e16a70
Dmitry Kombitsi <dmitrykombitsi@MacBook-Pro-Dmitry.local>
2021-06-25 15:40:42 +0300

    mcommit_add_choseNum

--- guess.go
+++ guess.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bufio"
 	_ "bufio"
 	"fmt"
 	_ "fmt"
+	"log"
 	_ "log"
 	"math/rand"
 	_ "math/rand"
+	"os"
 	_ "os"
+	"strconv"
 	_ "strconv"
+	"strings"
 	_ "strings"
 	"time"
 	_ "time"
@@ -21,22 +26,24 @@ func main() {
 	target := rand.Intn(100) + 5
 	fmt.Println("I've chosen a random number between 1 and 100.")
 	fmt.Println("Can you guess it?")
-	fmt.Println(target) }
+	fmt.Println(target)
 
-	reader := bufio.NewReader (os.Stdin) // блок для ввода чисел и проверки
+		reader := bufio.NewReader (os.Stdin) // блок для ввода чисел и проверки
 
-		fmt.Print("Введите число:")
-		input, err := reader.ReadString('/n')
-		if err != nul {
-			log.Fatal(err)
-		}
-		input = strings.TrimSpase(input) {
-			guess, err := strconv.Atoi(input)
-			if err != nil {
-			log.Fatal(err)
-			}
-		}
 		for guesses := 0; guesses < 10; guesses++ {
+			fmt.Println("У тебя есть", 10-guesses , "попыток.")
+
+			fmt.Print("Введите число:")
+			input, err := reader.ReadString('/n')
+			if err != nil {
+				log.Fatal(err)
+			}
+			input = strings.TrimSpace(input)
+				guess, err := strconv.Atoi(input)
+			if err != nil {
+				log.Fatal(err)
+
+			}
 			if guess < target {
 				fmt.Println("Oops. Ты ввел маленькое число.")
 			} else if guess > target {
